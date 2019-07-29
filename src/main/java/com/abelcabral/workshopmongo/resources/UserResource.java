9bf6215ea77afafa7aeeae0a194f3ddcd4439048
abel-cabral <jcabral@id.uff.br>
2019-07-29 00:36:54 -0300

    Create DTO user

--- src/main/java/com/abelcabral/workshopmongo/resources/UserResource.java
+++ src/main/java/com/abelcabral/workshopmongo/resources/UserResource.java
@@ -1,15 +1,15 @@
 package com.abelcabral.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.abelcabral.workshopmongo.domain.User;
+import com.abelcabral.workshopmongo.dto.UserDTO;
 import com.abelcabral.workshopmongo.services.UserService;
 
 @RestController // Para dizer que é uma classe que utilizara recurso rest precisamos usar
@@ -21,8 +21,13 @@ public class UserResource {
 	
 	//@GetMapping // Informamos qual método de acesso a essa funcao no caso um Get e a rota
 	@GetMapping("/users")
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);		// Formatamos nossa resposta para uma versao mais sofisticada
+		
+		// Precisamos converter de list User para list UserDTO, percorremos cada elemento preenchendo a lista DTO
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList()); 		
+		
+		// Formatamos nossa resposta para uma versao mais sofisticada
+		return ResponseEntity.ok().body(listDTO);		
 	}
 }
